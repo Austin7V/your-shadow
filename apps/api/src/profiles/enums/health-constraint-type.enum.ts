78ea721a4ebe7c0270d865a93970ada74bd327c6
Sergey Badin <sergeykfg1@me.com>
2026-08-25 19:03:57 +0200

    feat: define encrypted profile data models

---
+++ apps/api/src/profiles/enums/health-constraint-type.enum.ts
@@ -0,0 +1,8 @@
+export enum HealthConstraintType {
+  CHRONIC_CONDITION = 'chronic_condition',
+  INJURY = 'injury',
+  ALLERGY = 'allergy',
+  MOBILITY_LIMITATION = 'mobility_limitation',
+  MEDICAL_RESTRICTION = 'medical_restriction',
+  OTHER = 'other',
+}
